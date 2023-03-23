c133d7192a9504918a2f6334fa6c1e0531d9ed44
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-03-23 22:34:26 +0900

    cicd test#4

--- src/main/java/com/example/seoulcultureport/controller/BoardController.java
+++ src/main/java/com/example/seoulcultureport/controller/BoardController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
-public class BoardController {  //원트가능?//원트실패
+public class BoardController {  //원트가능?//원트실패//휴먼오류 미쳤다
 
     private final BoardService boardService;
     private final UserService userService;
